71c2a702ef456e38f8b045f04ae4d4793b3ed65b
kissoft <partner@kissoft.co.kr>
2016-05-27 14:57:55 +0900

    핑거푸시 v3.0 내용 추가

--- sendTargetOverMax.php
+++ sendTargetOverMax.php
@@ -18,10 +18,16 @@ $fp = new Fingerpush\Fingerpush ( $key );
 
 $arrayUser = array();
 $arrayMessage = array();
+$arrayImage = array();
+$arrayLink = array();
+$arrayTitle = array();
 
 for ($i=0; $i<=5000; $i++){
 	$arrayUser[$i] = 'user_'.$i;
 	$arrayMessage[$i] = $arrayUser[$i].'님 안녕하세요. 핑거푸시입니다.';
+	//$arrayImage[$i] = 'https://www.fingerpush.com/sample.jpg';
+	//$arrayLink[$i] = 'htts://www.fingerpush.com';
+	//$arrayTitle[$i] = $arrayUser[$i].'님을 위한 맞춤 푸시';
 }
 
 //$arrayMessage[] .= '에러유발';
@@ -29,6 +35,9 @@ for ($i=0; $i<=5000; $i++){
 $param = array (
 		'identity' => $arrayUser,
 		'message' => $arrayMessage,
+		// 'arrImage' => $arrayImage,
+		// 'arrLink' => $arrayLink,
+		// 'arrTitle' => $arrayTitle,
 		// 'isa' => '',
 		// 'abdg' => '',
 		// 'asnd' => '',
@@ -41,7 +50,21 @@ $param = array (
 		// 'cv1' => '',
 		// 'cv2' => '',
 		// 'cv3' => '',
-		// 'fnm' => ''
+		// 'fnm' => '',
+		// 'link' => '',
+        // 'mode' => 'DEFT',
+        // 'lngt_message' => '',
+        // 'send_state' => '0001',
+        // 'senddate' => '',
+        // 'title' => '',
+        // 'bgcolor' => '#FFFFFF',
+        // 'fcolor' => '#000000',
+        // 'lcode' => '',	
+	    // 'isetiquette' => 'Y', 
+	    // 'etiquette_stime' => '20',
+	    // 'etiquette_etime' => '8', 
+	    // 'and_priority' => 'M',
+	    // 'optagree' => '0000'
 );
 
 $fp->setParam ( $param );
