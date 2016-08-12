e9cbee8a81b3ad64c9531f1f09fd55893f650dd3
kissoft <partner@kissoft.co.kr>
2016-08-12 16:23:45 +0900

    curl SSL 인증 에러 검출
    curl SSL 인증 에러 검출

--- PHP_5.3.0/FingerPush/src/FingerPushRequest.php
+++ PHP_5.3.0/FingerPush/src/FingerPushRequest.php
@@ -411,6 +411,7 @@ class FingerPushRequest {
 		$ch = curl_init ();
 		curl_setopt ( $ch, CURLOPT_URL, $url );
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, false );
 		curl_setopt ( $ch, CURLOPT_SSLVERSION, 1 );
 		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
 		curl_setopt ( $ch, CURLOPT_POST, 1 );
